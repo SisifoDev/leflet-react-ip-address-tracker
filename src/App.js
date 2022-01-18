4864ee60a71c0a9a63f8191fb5914bb0ff85cdf5
Manuel Perdomo <manuelperdomo1710@icloud.com>
2022-01-18 07:37:26 -0500

    [Add] Breackpoints

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
         `https://geo.ipify.org/api/v2/country,city?apiKey=at_nESUW1ewhrZmgjNOxSERVgc28c0NE&ipAddress=${ip}`
       );
       setIpData(consulta.data);
+      console.log(consulta.data);
     };
     consultaAPI();
   }, [ip]);
@@ -45,18 +46,45 @@ function App() {
       <Main>
         <Search>
           <Title>IP Address Tracker</Title>
-
-          <Form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              value={inputValue}
-              onChange={handleInputChange}
-              placeholder="Search for any IP address or domain"
-            />
-            <Button onClick={handleSubmit}>
+          <FormWrapper>
+            <Form onSubmit={handleSubmit} id="searchForm">
+              <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                placeholder="Search for any IP address or domain"
+              />
+            </Form>
+            <Button onClick={handleSubmit} form="searchForm">
               <Arrow />
             </Button>
-          </Form>
+          </FormWrapper>
+          <Details>
+            <ContentDetails>
+              <h2>Ip Address</h2>
+              {isObjEmpty(ipData) ? "-- --" : <p>{ipData.ip}</p>}
+            </ContentDetails>
+            <ContentDetails>
+              <h2>Location</h2>
+              {isObjEmpty(ipData) ? (
+                "-- --"
+              ) : (
+                <p>{`${ipData.location.city}, ${ipData.location.region}, ${ipData.location.country}`}</p>
+              )}
+            </ContentDetails>
+            <ContentDetails>
+              <h2>Timezone</h2>
+              {isObjEmpty(ipData) ? (
+                "-- --"
+              ) : (
+                <p>{`UTC ${ipData.location.timezone}`}</p>
+              )}
+            </ContentDetails>
+            <ContentDetails>
+              <h2>ISP</h2>
+              {isObjEmpty(ipData) ? "-- --" : <p>{ipData.isp}</p>}
+            </ContentDetails>
+          </Details>
         </Search>
 
         <div id="map">
@@ -112,19 +140,24 @@ const Search = styled.section`
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
+
+  @media (min-width: 1400px) {
+    padding: 0 165px;
+  }
 `;
 
 const Title = styled.h1`
   color: #fff;
   font-size: 26px;
+  margin-top: 180px;
 `;
 
-const Form = styled.form`
+const FormWrapper = styled.div`
   width: 100%;
-
   height: 58px;
   display: flex;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
   background-color: #fff;
   border-radius: 15px;
@@ -132,8 +165,9 @@ const Form = styled.form`
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 
   input {
+    max-width: 500px;
     width: 100%;
-    height: 100%;
+    height: 58px;
     border: none;
     border-radius: 15px 0 0 15px;
     margin-left: 24px;
@@ -144,5 +178,80 @@ const Form = styled.form`
     line-height: 21px;
     outline: none;
     color: #2c2c2c;
+  }
+
+  @media (min-width: 1400px) {
+    width: 555px;
+  }
+`;
+
+const Form = styled.form`
+  width: 500px;
+  height: 100%;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 24px;
+  align-items: center;
+  border-radius: 15px;
+  padding: 24px;
+  height: auto;
+  width: 100%;
+  background: #fff;
+  position: relative;
+  bottom: -25px;
+  z-index: 9999;
+
+  & h2 {
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 10px;
+    line-height: 12px;
+    /* identical to box height */
+
+    /* text-align: center; */
+    letter-spacing: 1.45833px;
+    text-transform: uppercase;
+    opacity: 50%;
+  }
+
+  & p {
+    color: var(--veryDarkGray);
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 20px;
+    line-height: 24px;
+    /* identical to box height, or 120% */
+
+    text-align: center;
+    letter-spacing: -0.178571px;
+    @media (min-width: 1400px) {
+      justify-content: space-around;
+      font-size: 26px;
+      text-align: start;
+    }
+  }
+
+  @media (min-width: 1400px) {
+    flex-direction: row;
+    gap: 65px;
+    padding: 32px 37px;
+  }
+`;
+
+const ContentDetails = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 7px;
+  justify-content: center;
+  align-items: center;
+  @media (min-width: 1400px) {
+    align-items: flex-start;
+    gap: 13px;
   }
 `;
